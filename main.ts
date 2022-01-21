bd58b1d234569325c8f0e53fb37194b741a0e729
Captain Awesome Jnr <90992828+captainawesomejnr@users.noreply.github.com>
2022-01-22 08:10:31 +1100

    First release

--- main.ts
+++ main.ts
@@ -1,16 +1,62 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . # . # .
+            # # # # #
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+        basic.showLeds(`
+            . # . # .
+            # . # . #
+            # . . . #
+            . # . # .
+            . . # . .
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # # # .
+            . . # . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . # . # .
+            # # # # #
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+        basic.showLeds(`
+            . # . # .
+            # # # # #
+            # # . # #
+            . # # # .
+            . . # . .
+            `)
+        basic.showLeds(`
+            . # . # .
+            # . # . #
+            # . . . #
+            . # . # .
+            . . # . .
+            `)
+        basic.clearScreen()
+    }
+})
+let strip = neopixel.create(DigitalPin.P0, 24, NeoPixelMode.RGB)
 basic.forever(function () {
-    basic.showLeds(`
-        . # . # .
-        # # # # #
-        # # # # #
-        . # # # .
-        . . # . .
-        `)
-    basic.showLeds(`
-        . # . # .
-        # . # . #
-        # . . . #
-        . # . # .
-        . . # . .
-        `)
+    strip.showColor(neopixel.colors(NeoPixelColors.Red))
 })
