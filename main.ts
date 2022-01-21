2d532c5715c1fc638a61d3004e5c53f4981c4752
Captain Awesome Jnr <90992828+captainawesomejnr@users.noreply.github.com>
2022-01-22 08:37:49 +1100

    test functions added using joystick:bit

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P14, joystickbit.ButtonType.down, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
+})
 input.onButtonPressed(Button.A, function () {
     for (let index = 0; index < 4; index++) {
         basic.showLeds(`
@@ -15,6 +24,9 @@ input.onButtonPressed(Button.A, function () {
             . . # . .
             `)
     }
+})
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P15, joystickbit.ButtonType.down, function () {
+    joystickbit.Vibration_Motor(100)
 })
 input.onButtonPressed(Button.B, function () {
     for (let index = 0; index < 4; index++) {
@@ -56,6 +68,25 @@ input.onButtonPressed(Button.B, function () {
         basic.clearScreen()
     }
 })
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.down, function () {
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+})
+joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType.down, function () {
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+})
+joystickbit.initJoystickBit()
 let ButtonPressed = 0
 while (ButtonPressed == 0 && !(input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B))) {
     basic.showString("Press A or B to begin")
